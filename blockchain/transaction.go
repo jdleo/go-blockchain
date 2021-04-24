a71f031d27284b275b3a7e2eecde025616446541
John Leonardo <j@jdleo.me>
2021-04-24 00:22:06 -0500

    refactored block/blockchain/proof data to include txn logic

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/gob"
+	"encoding/hex"
 	"fmt"
+	"log"
 )
 
 type Transaction struct {
@@ -51,6 +53,38 @@ func CoinbaseTx(to, data string) *Transaction {
 	// create txn
 	tx := Transaction{nil, []TxInput{txin}, []TxOutput{txout}}
 	// set id
+	tx.SetID()
+
+	return &tx
+}
+
+func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction {
+	var inputs []TxInput
+	var outputs []TxOutput
+
+	acc, validOutputs := chain.FindSpendableOutputs(from, amount)
+
+	if acc < amount {
+		log.Panic("Error: not enough funds for this transaction")
+	}
+
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
+		Handle(err)
+
+		for _, out := range outs {
+			input := TxInput{txID, out, from}
+			inputs = append(inputs, input)
+		}
+	}
+
+	outputs = append(outputs, TxOutput{amount, to})
+
+	if acc > amount {
+		outputs = append(outputs, TxOutput{acc - amount, from})
+	}
+
+	tx := Transaction{nil, inputs, outputs}
 	tx.SetID()
 
 	return &tx
