407d050e61398b2b0337ed668a69cfff4969f3cd
John Leonardo <j@jdleo.me>
2021-04-24 12:07:21 -0500

    change back to cli in main

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"os"
 
-	wallet "github.com/jdleo/go-blockchain/wallet"
+	"github.com/jdleo/go-blockchain/cli"
 )
 
 func main() {
 	defer os.Exit(0)
-	// cmd := cli.CommandLine{}
-	// cmd.Run()
-
-	w := wallet.MakeWallet()
-	w.Address()
+	cmd := cli.CommandLine{}
+	cmd.Run()
 }
